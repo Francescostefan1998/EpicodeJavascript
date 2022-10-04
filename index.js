ecf3ccdaed69e9433429a244818a62a9b56e662e
Francesostefan1998 <francescostefan98@gmail.com>
2022-10-04 14:43:18 +0200

    javascriptprova
    javascriptprova

---
+++ index.js
@@ -0,0 +1,10 @@
+//creating variable for name and age//
+let myname = "Francesco"
+let mysurname = "Stefan"
+let space = " "
+let fullname = myname + space + mysurname
+console.log("Hello my name is ", fullname);
+let currentyear = 2022
+let myage = 24
+let yearofbirth = currentyear - myage
+console.log("Hello, my name is ", fullname, "and my age is ", myage);
